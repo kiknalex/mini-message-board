b65200c3088ecc952c641702944d2b3d8c09252b
Alex <aleksandrekiknadzee@gmail.com>
2024-08-30 18:14:16 +0400

    adjustments

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
 require('dotenv').config();
-module.exports = new Pool({
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  database: process.env.DATABASE,
-});
+// module.exports = new Pool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.PORT,
+//   database: process.env.DATABASE,
+// });
 
+module.exports = new Pool({connectionString: process.env.CONNECTION_URI});
